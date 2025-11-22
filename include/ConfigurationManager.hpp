29291ad8053b61a4ea51fc6129b86d698d67d986
LawlietDN <ufcgamer82@gmail.com>
2025-11-22 12:26:21 +0100

    introduced FeedEndpoint struct and replaced single static feed target with full list of eight MTA GTFS feed URLs plus a getter to expose them for multi-feed polling

--- include/ConfigurationManager.hpp
+++ include/ConfigurationManager.hpp
@@ -1,14 +1,36 @@
 #include <string>
+#include <vector>
+
+struct FeedEndpoint
+{
+    std::string name;
+    std::string url;
+    // Future: ProtocolType type; (PROTOBUF vs JSON)
+};
+
 
 class ConfigurationManager
 {
 private:
     std::string apiKey;
+    std::vector<FeedEndpoint> activeFeeds;
+    static inline const std::vector<std::string> FEED_URLS = {
+        "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace",  // A, C, E, H
+        "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs",      // 1, 2, 3, 4, 5, 6, S
+        "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw", // N, Q, R, W
+        "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm", // B, D, F, M
+        "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l",    // L
+        "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-sir",  // Staten Island
+        "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g",    // G
+        "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz"    // J, Z
+    };
+
 
 public:
     ConfigurationManager();
     static inline const std::string MTA_HOST   = "api-endpoint.mta.info";
     static inline const std::string MTA_PORT   = "443";
-    static inline const std::string MTA_TARGET = "/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace";
+
     [[nodiscard]] std::string getAPIKey() const noexcept;
+    [[nodiscard]] std::vector<FeedEndpoint> const& getFeeds() const noexcept;
 };
