4d3c54eba4cdb49646d5598f6491d7d5b7db2c0b
LawlietDN <ufcgamer82@gmail.com>
2025-11-22 20:18:47 +0100

    Declaration of dwellTimeSeconds of type int.

--- include/Types.hpp
+++ include/Types.hpp
@@ -13,7 +13,8 @@ struct TrainSnapshot
     bool isAssigned;         // Is it actually running?
     std::string stopId;       // "A12S" (Station Code)
     int32_t currentStatus;    // 0=INCOMING, 1=STOPPED_AT, 2=IN_TRANSIT_TO
-    int32_t delay = 0; 
+    int32_t delay = 0;
+    int dwellTimeSeconds = 0; 
 
     
 };
